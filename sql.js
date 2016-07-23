21de7c719f1131d2d2d0b5f71e23ab7a3136ea60
Joshua Bartlett <jcbart12595@gmail.com>
2016-07-23 16:56:56 -0700

    adding users to db

--- sql.js
+++ sql.js
@@ -7,13 +7,40 @@ var demoji_db = mysql.createConnection({
   database : 'sys'
 });
 
-demoji_db .connect(function(err){
+/*
+demoji_db.connect(function(err){
   if(err){
     console.log('Error connecting to Db');
     return;
   }
   console.log('Connection established we got db up');
 });
+*/
+
+/*
+demoji_db.query('SHOW COLUMNS FROM reactions', function(err, rows, fields){ 
+    
+    for(var i=0; i<rows.length; i++) {
+      console.log(rows[i].Field);
+    }
+});
+*/
+
+var addUser = function(username, gender, race, sexual_orientation, income, age, religion, city) {
+  demoji_db.query('INSERT INTO users (username, gender, race, sexual_orientation, income, age, religion, city)'+
+  'VALUES ('+username+', '+gender+', '+race+', '+sexual_orientation+', '+income+', '+age+', '+religion+', '+city+')', function(err, rows, fields){
+
+    if(err){
+      console.log(err);
+      return;
+    }
+
+    console.log('yay');
+
+  });
+}
+
+//addUser('"fredrick"', '"male"', '"black"', '"straight"', '"3983873942883"', '20', '"christian"', '"Accra"');
 
 demoji_db.end(function(err) {
   // The connection is terminated gracefully

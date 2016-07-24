7bff8a3ab5a5a3359cb6c447bab1f21c58980fbf
Fredrick-Tam <tamf295@yahoo.com>
2016-07-24 02:34:40 -0700

    preliminary work for huge func

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ require('./routes')(app);
 app.get('/', function(req, res) {
   res.send('Hello Seattle\n');
 });
-
+db.showColumns();
+db.getRelevantPicData('4','male');
 app.listen(3001);
 
 console.log('Listening on port 3001...');
--- sql.js
+++ sql.js
@@ -21,7 +21,7 @@ demoji_db.connect(function(err){
 
 
 exports.showColumns = function(){
-  demoji_db.query('SHOW COLUMNS FROM pictures', function(err, rows, fields){ 
+  demoji_db.query('SHOW COLUMNS FROM users', function(err, rows, fields){ 
     
     for(var i=0; i<rows.length; i++) {
       console.log(rows[i].Field);
@@ -85,6 +85,76 @@ exports.insertReaction = function(userId, imageId, reaction) {
 
     console.log('updated');
   });
+}
+
+exports.getRelevantPicData = function(imageId,demoData) {
+	demoji_db.query('SELECT reactions.image_id, users.id,users.username, reactions.reaction, users.city, users.sexual_orientation, users.race,users.age, users.gender, users.income, users.religion FROM reactions INNER JOIN users ON reactions.user_id = users.id WHERE image_id='+imageId, function(err, rows, fields) {
+    if(err){
+      console.log(err);
+      return;
+    }
+
+    demographicImageDataMapping(demoData);
+    console.log('sweet');
+});
+
+}
+
+var demographicImageData = function(demoData) {
+switch (demoData) {
+    case users.["gender"].ToString():
+        demoji_db.query('SELECT reactions.image_id, users.id,users.username, reactions.reaction, users.city, users.sexual_orientation, users.race,users.age, users.gender, users.income, users.religion FROM reactions INNER JOIN users ON reactions.user_id = users.id WHERE image_id='+imageId, function(err, rows, fields) {
+    	if(err){
+      	console.log(err);
+      	return;
+    	};
+        break;
+    case users.["age"]:
+        demoji_db.query('SELECT reactions.image_id, users.id,users.username, reactions.reaction, users.city, users.sexual_orientation, users.race,users.age, users.gender, users.income, users.religion FROM reactions INNER JOIN users ON reactions.user_id = users.id WHERE image_id='+imageId, function(err, rows, fields) {
+    	if(err){
+      	console.log(err);
+      	return;
+    	};
+        break;
+    case users.["race"].ToString():
+        demoji_db.query('SELECT reactions.image_id, users.id,users.username, reactions.reaction, users.city, users.sexual_orientation, users.race,users.age, users.gender, users.income, users.religion FROM reactions INNER JOIN users ON reactions.user_id = users.id WHERE image_id='+imageId, function(err, rows, fields) {
+    if(err){
+      console.log(err);
+      return;
+    }
+      break;
+    case users.["religion"].ToString():
+       demoji_db.query('SELECT reactions.image_id, users.id,users.username, reactions.reaction, users.city, users.sexual_orientation, users.race,users.age, users.gender, users.income, users.religion FROM reactions INNER JOIN users ON reactions.user_id = users.id WHERE image_id='+imageId, function(err, rows, fields) {
+    if(err){
+      console.log(err);
+      return;
+    }
+        break;
+    case users.["sexual_orientation"].ToString():
+      demoji_db.query('SELECT reactions.image_id, users.id,users.username, reactions.reaction, users.city, users.sexual_orientation, users.race,users.age, users.gender, users.income, users.religion FROM reactions INNER JOIN users ON reactions.user_id = users.id WHERE image_id='+imageId, function(err, rows, fields) {
+    if(err){
+      console.log(err);
+      return;
+    }
+        break;
+    case  users.["city"].ToString():
+        demoji_db.query('SELECT reactions.image_id, users.id,users.username, reactions.reaction, users.city, users.sexual_orientation, users.race,users.age, users.gender, users.income, users.religion FROM reactions INNER JOIN users ON reactions.user_id = users.id WHERE image_id='+imageId, function(err, rows, fields) {
+    if(err){
+      console.log(err);
+      return;
+    }
+    case  users.["income"].ToString():
+        demoji_db.query('SELECT reactions.image_id, users.id,users.username, reactions.reaction, users.city, users.sexual_orientation, users.race,users.age, users.gender, users.income, users.religion FROM reactions INNER JOIN users ON reactions.user_id = users.id WHERE image_id='+imageId, function(err, rows, fields) {
+    if(err){
+      console.log(err);
+      return;
+    }
+}
+
+
+
+
+	console.log("lolol");
 }
 
 /*
